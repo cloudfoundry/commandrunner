a8d47a45b23fba6c27fe0e50975b206672cc099a
Glyn Normington <gnormington@pivotal.io>
2017-02-20 16:46:12 +0000

    Add HaveExecuted matcher
    Also needed to change package.go for this to compile.
    
    closes #3

--- fake_command_runner/matchers/package.go
+++ fake_command_runner/matchers/package.go
@@ -1,1 +1,1 @@
-package matchers // import "code.cloudfoundry.org/commandrunner/fake_command_runner/matchers"
+package fake_command_runner_matchers // import "code.cloudfoundry.org/commandrunner/fake_command_runner/matchers"
